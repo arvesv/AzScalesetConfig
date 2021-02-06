152f8d50534f106c485bd1666451b7fa869c405e
Arve Svendsen <arve.svendsen@gmail.com>
2021-02-06 03:55:25 +0100

    First version of software script

---
+++ WindowsAgent/extension.ps1
@@ -0,0 +1,5 @@
+# Set the path for the work area 
+mkdir D:\a
+[System.Environment]::SetEnvironmentVariable('VSTS_AGENT_INPUT_WORK', 'd:\a')
+
+
---
+++ WindowsAgent/software.ps1
@@ -0,0 +1,7 @@
+# Some of the software installed on the build agents
+# choco upgrade either installs or upgrades
+
+choco upgrade 7zip -y
+choco upgrade golang -y 
+choco upgrade azure-cli -y
+

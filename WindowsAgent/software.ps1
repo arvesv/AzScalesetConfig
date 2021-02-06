29d05d48a3cfb9cea7b76235bb4e667359d15d89
Arve Svendsen <arve.svendsen@gmail.com>
2021-02-06 04:23:20 +0100

    First version of software script

--- WindowsAgent/software.ps1
+++ WindowsAgent/software.ps1
@@ -4,4 +4,19 @@
 choco upgrade 7zip -y
 choco upgrade golang -y 
 choco upgrade azure-cli -y
+choco upgrade powershell-core -y
+choco upgrade git -y
+choco upgrade pulumi -y
+choco install openjdk -y
+choco upgrade dotnet-sdk -y
+choco upgrade dotnetcore-sdk -y
+choco upgrade wixtoolset -y
+choco upgrade python3 -y
+choco install nodejs-lts -y
+
+
+
+
+
+
 
